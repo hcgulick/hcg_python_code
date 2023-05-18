acf074bc3f08a80f65bc4f768dc799e3999ad276
hcgulick <hannah_gulick@berkeley.edu>
2023-05-18 15:26:37 -0700

    added function to plot light curve (image summed flux) for multiple surveys

--- kepler_analysis/kepler_analysis_code.py
+++ kepler_analysis/kepler_analysis_code.py
@@ -14,7 +14,7 @@ from astroquery.mast import Observations
 
 ### DEFINITIONS ###
 
-def open_file(filename):
+def open_file(filename, add_back = 'no'):
     
     h = pyfits.open(filename)
     data = h[1].data
@@ -22,17 +22,22 @@ def open_file(filename):
     #print(h.info())
     h.close() # close the file
     
-    return data, hdr
-
-def filter_nan(data):
-    
-    '''remove nan from time, flux, and flux_err as well
-    as flux = zero data points'''
-    
     time = np.array(data['TIME']).astype(float)
     flux = np.array(data['FLUX']).astype(float)
     flux_err = np.array(data['FLUX_ERR']).astype(float)
-    back_flux = np.array(data['FLUX_BKG']).astype(float)
+    
+    if add_back != 'no':
+        print('Adding background')
+        back_flux = np.array(data['FLUX_BKG']).astype(float)
+        mean_back_flux_frame = np.nanmean(np.array(back_flux), axis = 0)
+        flux = flux+mean_back_flux_frame
+    
+    return time, flux, flux_err, hdr
+    
+def filter_parameters(time, flux, flux_err):
+    
+    '''remove nan from time, flux, and flux_err as well
+    as flux = zero data points'''
     
     #finding the index of the middle of the image
     half_row_len = int(len(flux[0])/2)
@@ -46,7 +51,6 @@ def filter_nan(data):
     time = time[time_isnan]
     flux = flux[time_isnan]
     flux_err = flux_err[time_isnan]
-    back_flux = back_flux[time_isnan]
     
     flux_isnan = []
     for i in flux:
@@ -58,7 +62,6 @@ def filter_nan(data):
     time = time[flux_isnan]
     flux = flux[flux_isnan]
     flux_err = flux_err[flux_isnan]
-    back_flux = back_flux[flux_isnan]
     
     flux_err_isnan = []
     for i in flux_err:
@@ -67,11 +70,8 @@ def filter_nan(data):
     time = time[flux_err_isnan]
     flux = flux[flux_err_isnan]
     flux_err = flux_err[flux_err_isnan]
-    back_flux = back_flux[flux_err_isnan]
     
-    mean_back_flux_frame = np.nanmean(np.array(back_flux), axis = 0)
-    
-    return time, flux, flux_err, mean_back_flux_frame
+    return time, flux, flux_err
     
 def get_wcs_coords(hdr):
     
@@ -92,13 +92,109 @@ def get_wcs_coords(hdr):
     wcs_dict = WCS(wcs_input_dict)
     
     return wcs_dict
+    
+def sum_img_flux(time, flux, flux_err, plot = 'no', savefig_filename = 'no'):
+    
+    time_filt, flux_filt, flux_err_filt = filter_parameters(time, flux, flux_err)
+    
+    flux_filt_sum = []
+    for i in flux_filt:
+        flux_sum_row = []
+        for j in i:
+            flux_sum_row.append(sum(j))
+        flux_sum_row = np.nan_to_num(flux_sum_row, nan = 0.0)
+        flux_filt_sum.append(sum(flux_sum_row))
+    
+    if plot == 'yes':
+        plt.figure(figsize = (9, 5))
+        #plt.title('Light Curve Pixel(row, col) = (%s, %s)' %(row_ind, col_ind))
+        plt.plot(time_filt, flux_filt_sum, color = 'purple')
+        plt.xlabel('Time', fontsize = 15)
+        plt.ylabel('Sum of Image Flux', fontsize = 15)
+        plt.xticks(fontsize = 13)
+        plt.yticks(fontsize = 13)
+        if savefig_filename != 'no':
+            plt.savefig(savefig_filename)
+        plt.show()
+    
+    #frame_ave_flux_sum = np.array(flux_sum)/(len(flux))
+    
+    return time_filt, flux_filt_sum
+    
+
+def calc_flux_over_area(time, flux, flux_err, ind, sum_area):
+    
+    time_filt, flux_filt, flux_err_filt = filter_parameters(time[ind], flux[ind], flux_err[ind])
+    
+    #finding middle coord of first image
+    half_row_len = int(len(flux[0])/2)
+    half_col_len = int(len(flux[0][0])/2)
+
+    summed_pupil_fluxes = []
+    for i in range(len(flux_filt)):
+        img_flux = flux_filt[i]
+        
+        row_fluxes = img_flux[int(half_row_len-sum_area/2):int(half_row_len+sum_area/2)]
+        pupil = row_fluxes.T[int(half_col_len-sum_area/2):int(half_col_len+sum_area/2)]
+        summed_pupil_fluxes.append(np.sum(pupil))
+        #print('percent done:', i/len(flux))
+    
+    return summed_pupil_fluxes
+    
+def calc_sample_frequency(time, flux, flux_err, ind):
+    
+    time_filt, flux_filt, flux_err_filt = filter_parameters(time[ind], flux[ind], flux_err[ind])
+    
+    time_steps = []
+    for i in range(len(time_filt)-1):
+        time_steps.append(time_filt[i+1]-time_filt[i])
+    
+    frequency = 1/np.median(time_steps)
+    
+    return frequency
+
+def make_periodogram(time, flux, flux_err, filename_all, start_ind, file_num, sum_area, savefig = 'no'):
+
+    f_all = []
+    Pxx_den_all = []
+    fnames = []
+
+    ind = start_ind
+    for i in range(start_ind, start_ind+file_num):
+        #calculating light curves over pupil area
+        lc = calc_flux_over_area(time, flux, flux_err, ind, sum_area)
+        #calculating observation frequency
+        freq = calc_sample_frequency(time, flux, flux_err, ind)
+        #calculating periodogram signal
+        f, Pxx_den = signal.periodogram(lc, freq)
+        f_all.append(f)
+        fnames.append(filenames_all[ind])
+        Pxx_den_all.append(Pxx_den)
+        ind += int(len(time)/3)
+        
+        
+    
+    fig, axs = plt.subplots(1, file_num, figsize = (13,3))
+    
+    axs_all = []
+    for i in range(file_num):
+        a = axs[i].plot(f_all[i], Pxx_den_all[i], color = 'black', linewidth = 1)
+        axs[i].set_title(fnames[i][:-9])
+        axs[i].set_xlabel('frequency [Hz]')
+        axs_all.append(a)
+        
+    axs[0].set_ylabel('PSD [V**2/Hz]')
+    plt.show()
 
 
-def plot_image(binarytab, num_im, hdr):
+
+
+
+######## Plotting functions ###########
+
+def plot_image(flux, num_im, hdr):
     
     wcs_dict = get_wcs_coords(hdr)
-    
-    flux = binarytab['FLUX']
 
     for i in range(num_im):
         #plt.xlabel('Column')
@@ -118,17 +214,17 @@ def plot_image(binarytab, num_im, hdr):
         plt.title('Flux Image: %s' %(i))
         plt.show()
         
-def plot_light_curve(binarytab, row_ind, col_ind):
+def plot_light_curve(time, flux, flux_err, row_ind, col_ind):
 
-    time, flux, flux_err, back_flux = filter_nan(binarytab)
+    time_filt, flux_filt, flux_err_filt = filter_parameters(time, flux, flux_err)
     
     star_pix = []
     star_pix_err = []
     star_time = []
-    for i in range(len(flux)):
-        star_pix.append(flux[i][row_ind][col_ind])
-        star_pix_err.append(flux_err[i][row_ind][col_ind])
-        star_time.append(time[i])
+    for i in range(len(flux_filt)):
+        star_pix.append(flux_filt[i][row_ind][col_ind])
+        star_pix_err.append(flux_err_filt[i][row_ind][col_ind])
+        star_time.append(time_filt[i])
     
     plt.figure(figsize = (9, 5))
     plt.title('Light Curve Pixel(row, col) = (%s, %s)' %(row_ind, col_ind))
@@ -139,160 +235,51 @@ def plot_light_curve(binarytab, row_ind, col_ind):
     plt.yticks(fontsize = 13)
     #plt.legend()
     plt.show()
-    
 
-'''def sum_flux(img_set, plot = 'no', savefig_filename = 'no'):
-    
-    time, flux, flux_err, back_flux  = filter_nan(img_set)
-    
-    flux_sum = []
-    for i in flux:
-        flux_sum_row = []
-        for j in i:
-            flux_sum_row.append(sum(j))
-        flux_sum_row = np.nan_to_num(flux_sum_row, nan = 0.0)
-        flux_sum.append(sum(flux_sum_row))
-    
-    if plot == 'yes':
-        plt.figure(figsize = (9, 5))
-        #plt.title('Light Curve Pixel(row, col) = (%s, %s)' %(row_ind, col_ind))
-        plt.plot(time, flux_sum, color = 'purple')
-        plt.xlabel('Time', fontsize = 15)
-        plt.ylabel('Sum of Image Flux', fontsize = 15)
-        plt.xticks(fontsize = 13)
-        plt.yticks(fontsize = 13)
-        if savefig_filename != 'no':
-            plt.savefig(savefig_filename)
-        plt.show()
-        
-    #frame_ave_flux_sum = np.array(flux_sum)/(len(flux))
-    
-    
-    return time, flux_sum'''
-    
-def sum_flux(img_set, plot = 'no', savefig_filename = 'no', add_back = 'no'):
-    
-    time, flux, flux_err, back_flux = filter_nan(img_set)
-    
-    back_add_flux = flux+back_flux
-    
-    if add_back == 'yes':
-        flux_sum = []
-        for i in back_add_flux:
-            flux_sum_row = []
-            for j in i:
-                flux_sum_row.append(sum(j))
-            flux_sum_row = np.nan_to_num(flux_sum_row, nan = 0.0)
-            flux_sum.append(sum(flux_sum_row))
-    else:
-        flux_sum = []
-        for i in flux:
-            flux_sum_row = []
-            for j in i:
-                flux_sum_row.append(sum(j))
-            flux_sum_row = np.nan_to_num(flux_sum_row, nan = 0.0)
-            flux_sum.append(sum(flux_sum_row))
-    
-    if plot == 'yes':
-        plt.figure(figsize = (9, 5))
-        #plt.title('Light Curve Pixel(row, col) = (%s, %s)' %(row_ind, col_ind))
-        plt.plot(time, flux_sum, color = 'purple')
-        plt.xlabel('Time', fontsize = 15)
-        plt.ylabel('Sum of Image Flux', fontsize = 15)
-        plt.xticks(fontsize = 13)
-        plt.yticks(fontsize = 13)
-        if savefig_filename != 'no':
-            plt.savefig(savefig_filename)
-        plt.show()
-    
-    #frame_ave_flux_sum = np.array(flux_sum)/(len(flux))
-    
-    return time, flux_sum
-    
 
-def calc_flux_over_area(ind, sum_area, data_all):
-    
-    time, flux, flux_err, back_flux = filter_nan(data_all[ind])
-    
-    #finding middle coord of first image
-    half_row_len = int(len(flux[0])/2)
-    half_col_len = int(len(flux[0][0])/2)
+def plot_summed_lc_multiple_surveys(time, flux, flux_err, start_ind, file_num, filenames, file_paths, plot = 'yes', savefig = 'no'):
 
-    summed_pupil_fluxes = []
-    for i in range(len(flux)):
-        img_flux = flux[i]
-        
-        row_fluxes = img_flux[int(half_row_len-sum_area/2):int(half_row_len+sum_area/2)]
-        pupil = row_fluxes.T[int(half_col_len-sum_area/2):int(half_col_len+sum_area/2)]
-        summed_pupil_fluxes.append(np.sum(pupil))
-        #print('percent done:', i/len(flux))
-    
-    return summed_pupil_fluxes
-    
-def calc_sample_frequency(ind, data_all):
-    
-    time, flux, flux_err, back_flux = filter_nan(data_all[ind])
-    
-    time_steps = []
-    for i in range(len(time)-1):
-        time_steps.append(time[i+1]-time[i])
-    
-    frequency = 1/np.median(time_steps)
-    
-    return frequency
-
-def make_periodogram(start_ind, file_num, sum_area, data_all, filenames_all, savefig = 'no'):
-
-    f_all = []
-    Pxx_den_all = []
+    fsum_all = []
+    time_all = []
     fnames = []
 
     ind = start_ind
     for i in range(start_ind, start_ind+file_num):
-        print(ind)
-        #calculating light curves over pupil area
-        lc = calc_flux_over_area(ind, sum_area, data_all)
-        #calculating observation frequency
-        freq = calc_sample_frequency(ind, data_all)
-        #calculating periodogram signal
-        f, Pxx_den = signal.periodogram(lc, freq)
-        f_all.append(f)
-        fnames.append(filenames_all[ind])
-        Pxx_den_all.append(Pxx_den)
-        ind += int(len(data_all)/3)
-        
-        
+        time_filt, fsum_filt = sum_img_flux(time[ind], flux[ind], flux_err[ind])
+        fsum_all.append(fsum_filt)
+        time_all.append(time_filt)
+        fnames.append(filenames[ind])
+        ind += int(len(time)/3)
+
+    colors = ['slateblue', 'darkorange', 'pink', 'darkgreen', 'cyan', 'blue']
+    plt.figure(figsize = (11,8))
+    for i in range(len(fsum_all)):
+        plt.plot(time_all[i], fsum_all[i], color = colors[i], alpha = 0.7, label = fnames[i])
     
-    fig, axs = plt.subplots(1, file_num, figsize = (13,3))
-    
-    axs_all = []
-    for i in range(file_num):
-        a = axs[i].plot(f_all[i], Pxx_den_all[i], color = 'black', linewidth = 1)
-        axs[i].set_title(fnames[i][:-9])
-        axs[i].set_xlabel('frequency [Hz]')
-        axs_all.append(a)
-        
-    axs[0].set_ylabel('PSD [V**2/Hz]')
+    plt.xlabel('Time (days)', fontsize = 21)
+    plt.ylabel('Flux (e-/s)', fontsize = 21)
+    plt.xticks(fontsize = 19)
+    plt.yticks(fontsize = 19)
+    plt.legend(fontsize = 15)
+    if savefig != 'no':
+        plt.savefig(os.path.join(file_path_all[0], fnames[0][:-13]+'_full_obs_summed_flux.png'))
     plt.show()
 
+######### execute codes ##########
 
 
-
-######### execute codes
-
-
-def execute_lc(filename, row_ind, col_ind, num_im = 1):
+def execute_lc(filename, row_ind, col_ind, num_im = 1, add_back = 'no'):
     
-    data, hdr = open_file(filename)
-    plot_image(data, num_im, hdr)
-    plot_light_curve(data, row_ind, col_ind)
+    time, flux, flux_err, hdr = open_file(filename, add_back)
+    plot_image(flux, num_im, hdr)
+    plot_light_curve(time, flux, flux_err, row_ind, col_ind)
     
-def execute_img_sum(filename, add_back = 'no', plot = 'no', savefig_filename = 'no'):
+def execute_img_sum(filename, plot = 'no', savefig_filename = 'no', add_back = 'no'):
     
-    data, hdr = open_file(filename)
-    time, fsum = sum_flux(data, plot, savefig_filename, add_back)
+    time, flux, flux_err, hdr = open_file(filename, add_back)
+    time_filt, fsum_filt = sum_img_flux(time, flux_err, plot, savefig_filename)
     
-    return time, fsum
+    return time_filt, fsum_filt
 
 def execute_all(filename, row_ind, col_ind, num_im = 1, plot = 'no'):
     
